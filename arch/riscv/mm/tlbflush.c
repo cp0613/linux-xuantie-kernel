ad7048b8136813950a7c73870f7cd6683e0843c0
Alexandre Ghiti <alexghiti@rivosinc.com>
2024-01-10 08:48:56 +0000

    riscv: Call secondary mmu notifier when flushing the tlb
    This is required to allow the IOMMU driver to correctly flush its own
    TLB.
    
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Change-Id: Ia7a588d20f738a467ca5e62401428bf066baa95b

--- arch/riscv/mm/tlbflush.c
+++ arch/riscv/mm/tlbflush.c
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-2.0
-
 #include <linux/mm.h>
 #include <linux/smp.h>
 #include <linux/sched.h>
+#include <linux/mmu_notifier.h>
 #include <asm/sbi.h>
 #include <asm/mmu_context.h>
 #include <asm/cpufeature.h>
@@ -37,7 +37,6 @@ static inline void local_flush_tlb_all_asid(unsigned long asid)
 	else
 		local_flush_tlb_all();
 }
-
 static inline void local_flush_tlb_page_asid(unsigned long addr,
 		unsigned long asid)
 {
@@ -49,13 +48,11 @@ static inline void local_flush_tlb_page_asid(unsigned long addr,
 	else
 		local_flush_tlb_page(addr);
 }
-
 /*
  * Flush entire TLB if number of entries to be flushed is greater
  * than the threshold below.
  */
 static unsigned long tlb_flush_all_threshold __read_mostly = 64;
-
 static void local_flush_tlb_range_threshold_asid(unsigned long start,
 						 unsigned long size,
 						 unsigned long stride,
@@ -63,7 +60,6 @@ static void local_flush_tlb_range_threshold_asid(unsigned long start,
 {
 	unsigned long nr_ptes_in_range = DIV_ROUND_UP(size, stride);
 	int i;
-
 	if (nr_ptes_in_range > tlb_flush_all_threshold) {
 		local_flush_tlb_all_asid(asid);
 		return;
@@ -84,7 +80,6 @@ static void local_flush_tlb_range_threshold_asid(unsigned long start,
 		start += stride;
 	}
 }
-
 static inline void local_flush_tlb_range_asid(unsigned long start,
 		unsigned long size, unsigned long stride, unsigned long asid)
 {
@@ -95,18 +90,15 @@ static inline void local_flush_tlb_range_asid(unsigned long start,
 	else
 		local_flush_tlb_range_threshold_asid(start, size, stride, asid);
 }
-
 /* Flush a range of kernel pages without broadcasting */
 void local_flush_tlb_kernel_range(unsigned long start, unsigned long end)
 {
 	local_flush_tlb_range_asid(start, end - start, PAGE_SIZE, FLUSH_TLB_NO_ASID);
 }
-
 static void __ipi_flush_tlb_all(void *info)
 {
 	local_flush_tlb_all();
 }
-
 void flush_tlb_all(void)
 {
 	if (riscv_use_ipi_for_rfence())
@@ -114,21 +106,17 @@ void flush_tlb_all(void)
 	else
 		sbi_remote_sfence_vma_asid(NULL, 0, FLUSH_TLB_MAX_SIZE, FLUSH_TLB_NO_ASID);
 }
-
 struct flush_tlb_range_data {
 	unsigned long asid;
 	unsigned long start;
 	unsigned long size;
 	unsigned long stride;
 };
-
 static void __ipi_flush_tlb_range_asid(void *info)
 {
 	struct flush_tlb_range_data *d = info;
-
 	local_flush_tlb_range_asid(d->start, d->size, d->stride, d->asid);
 }
-
 static void __flush_tlb_range(struct mm_struct *mm, unsigned long start,
 			      unsigned long size, unsigned long stride)
 {
@@ -136,25 +124,20 @@ static void __flush_tlb_range(struct mm_struct *mm, unsigned long start,
 	const struct cpumask *cmask;
 	unsigned long asid = FLUSH_TLB_NO_ASID;
 	bool broadcast;
-
 	if (mm) {
 		unsigned int cpuid;
-
 		cmask = mm_cpumask(mm);
 		if (cpumask_empty(cmask))
 			return;
-
 		cpuid = get_cpu();
 		/* check if the tlbflush needs to be sent to other CPUs */
 		broadcast = cpumask_any_but(cmask, cpuid) < nr_cpu_ids;
-
 		if (static_branch_unlikely(&use_asid_allocator))
 			asid = atomic_long_read(&mm->context.id) & asid_mask;
 	} else {
 		cmask = cpu_online_mask;
 		broadcast = true;
 	}
-
 	if (broadcast) {
 		if (riscv_use_ipi_for_rfence()) {
 			ftd.asid = asid;
@@ -170,39 +153,34 @@ static void __flush_tlb_range(struct mm_struct *mm, unsigned long start,
 	} else {
 		local_flush_tlb_range_asid(start, size, stride, asid);
 	}
-
-	if (mm)
+	if (mm) {
 		put_cpu();
+	        mmu_notifier_arch_invalidate_secondary_tlbs(mm, start, start + size);
+	}
 }
-
 void flush_tlb_mm(struct mm_struct *mm)
 {
 	__flush_tlb_range(mm, 0, FLUSH_TLB_MAX_SIZE, PAGE_SIZE);
 }
-
 void flush_tlb_mm_range(struct mm_struct *mm,
 			unsigned long start, unsigned long end,
 			unsigned int page_size)
 {
 	__flush_tlb_range(mm, start, end - start, page_size);
 }
-
 void flush_tlb_page(struct vm_area_struct *vma, unsigned long addr)
 {
 	__flush_tlb_range(vma->vm_mm, addr, PAGE_SIZE, PAGE_SIZE);
 }
-
 void flush_tlb_range(struct vm_area_struct *vma, unsigned long start,
 		     unsigned long end)
 {
 	__flush_tlb_range(vma->vm_mm, start, end - start, PAGE_SIZE);
 }
-
 void flush_tlb_kernel_range(unsigned long start, unsigned long end)
 {
 	__flush_tlb_range(NULL, start, end - start, PAGE_SIZE);
 }
-
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 void flush_pmd_tlb_range(struct vm_area_struct *vma, unsigned long start,
 			unsigned long end)
