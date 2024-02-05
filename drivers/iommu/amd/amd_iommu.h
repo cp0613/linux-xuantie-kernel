66a0a2f51301e46c4cd888330a9b7ca110eade13
Vasant Hegde <vasant.hegde@amd.com>
2024-02-05 11:56:05 +0000

    iommu/amd: Add support for device based TLB invalidation
    Add support to invalidate TLB/IOTLB for the given device.
    
    These functions will be used in subsequent patches where we will
    introduce per device GCR3 table and SVA support.
    
    Signed-off-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20240205115615.6053-8-vasant.hegde@amd.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Change-Id: I9050565a6db41536da7c00c000a95d050e38c42e

--- drivers/iommu/amd/amd_iommu.h
+++ drivers/iommu/amd/amd_iommu.h
@@ -51,7 +51,13 @@ int amd_iommu_flush_page(struct iommu_domain *dom, u32 pasid, u64 address);
 void amd_iommu_update_and_flush_device_table(struct protection_domain *domain);
 void amd_iommu_domain_update(struct protection_domain *domain);
 void amd_iommu_domain_flush_complete(struct protection_domain *domain);
-void amd_iommu_domain_flush_tlb_pde(struct protection_domain *domain);
+void amd_iommu_domain_flush_pages(struct protection_domain *domain,
+				  u64 address, size_t size);
+void amd_iommu_dev_flush_pasid_pages(struct iommu_dev_data *dev_data,
+				     ioasid_t pasid, u64 address, size_t size);
+void amd_iommu_dev_flush_pasid_all(struct iommu_dev_data *dev_data,
+				   ioasid_t pasid);
+
 int amd_iommu_flush_tlb(struct iommu_domain *dom, u32 pasid);
 int amd_iommu_domain_set_gcr3(struct iommu_domain *dom, u32 pasid,
 			      unsigned long cr3);
