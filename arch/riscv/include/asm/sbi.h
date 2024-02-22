c59a0fb5e83d404c358280c9e93b9ec381b62806
Himanshu Chauhan <hchauhan@ventanamicro.com>
2024-02-22 18:20:58 +0530

    riscv: Add SBI debug trigger extension and function ids
    Debug trigger extension is a proposed SBI extension to support
    native debugging in S-mode and VS-mode.
    
    The proposal for the extension can be found at:
    https://lists.riscv.org/g/sig-hypervisors/message/361
    
    This patch adds the extension and the function IDs defined
    by the extension.
    
    Signed-off-by: Himanshu Chauhan <hchauhan@ventanamicro.com>
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>
    Change-Id: I5131e107c2deaaf39436a6504d847183a27a596f

--- arch/riscv/include/asm/sbi.h
+++ arch/riscv/include/asm/sbi.h
@@ -35,6 +35,7 @@ enum sbi_ext_id {
 	SBI_EXT_STA = 0x535441,
 	SBI_EXT_FWFT = 0x46574654,
 	SBI_EXT_PVLOCK = 0xAB0401,
+	SBI_EXT_DBTR = 0x44425452,
 
 	/* Experimentals extensions must lie within this range */
 	SBI_EXT_EXPERIMENTAL_START = 0x08000000,
@@ -131,6 +132,34 @@ enum sbi_ext_susp_fid {
 
 enum sbi_ext_susp_sleep_type {
 	SBI_SUSP_SLEEP_TYPE_SUSPEND_TO_RAM = 0,
+};
+
+/* SBI debug triggers function IDs */
+enum sbi_ext_dbtr_fid {
+	SBI_EXT_DBTR_NUM_TRIGGERS = 0,
+	SBI_EXT_DBTR_SETUP_SHMEM,
+	SBI_EXT_DBTR_TRIGGER_READ,
+	SBI_EXT_DBTR_TRIGGER_INSTALL,
+	SBI_EXT_DBTR_TRIGGER_UPDATE,
+	SBI_EXT_DBTR_TRIGGER_UNINSTALL,
+	SBI_EXT_DBTR_TRIGGER_ENABLE,
+	SBI_EXT_DBTR_TRIGGER_DISABLE,
+};
+
+struct sbi_dbtr_data_msg {
+	unsigned long tstate;
+	unsigned long tdata1;
+	unsigned long tdata2;
+	unsigned long tdata3;
+};
+
+struct sbi_dbtr_id_msg {
+	unsigned long idx;
+};
+
+struct sbi_dbtr_shmem_entry {
+	struct sbi_dbtr_data_msg data;
+	struct sbi_dbtr_id_msg id;
 };
 
 enum sbi_ext_pmu_fid {
