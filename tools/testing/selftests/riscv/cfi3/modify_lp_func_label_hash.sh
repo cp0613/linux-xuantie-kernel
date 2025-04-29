2a09063a8d8d7f0f1e39f22406f9ec40e336af29
lucz <chongzhi.lcz@alibaba-inc.com>
2025-04-29 10:03:06 +0800

    selftests: riscv: implement ss and lp tests in cfi3
    Signed-off-by: lucz <chongzhi.lcz@alibaba-inc.com>
    Change-Id: I52e1377b225ea3b8bab61ed74910f4cd250be7a4

---
+++ tools/testing/selftests/riscv/cfi3/modify_lp_func_label_hash.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-2.0
+
+# input params
+default_prog_name="riscv_cfi_test"
+default_func_name="test_func0"
+
+prog_name="${1:-$default_prog_name}"
+func_name="${2:-$default_func_name}"
+
+func_lp_patch='\x17\x00\x12\x34'
+
+func_addr_hex=$(/usr/bin/env readelf -s $prog_name | grep $func_name | awk '{print $2}')
+text_addr_hex=$(/usr/bin/env readelf -S $prog_name | grep text| awk '{print $5}')
+text_offs_hex=$(/usr/bin/env readelf -S $prog_name | grep text| awk '{print $6}')
+printf "func_addr_hex=0x%s, text_addr_hex=0x%s, text_offs_hex=0x%s\n" $func_addr_hex $text_addr_hex $text_offs_hex
+
+func_addr_dec=$((16#$func_addr_hex))
+text_addr_dec=$((16#$text_addr_hex))
+text_offs_dec=$((16#$text_offs_hex))
+printf "func_addr_hex=%d, text_addr_hex=%d, text_offs_hex=%d\n" $func_addr_dec $text_addr_dec $text_offs_dec
+
+func_file_offset=$((func_addr_dec - text_addr_dec + text_offs_dec))
+printf "func_file_offset=0x%X (%d)\n" $func_file_offset $func_file_offset
+
+echo -ne $func_lp_patch > lp_patch.bin
+
+echo "dd if=lp_patch.bin of=$prog_name bs=1 seek=$(($func_file_offset)) conv=notrunc"
+/usr/bin/env dd if=lp_patch.bin of=$prog_name bs=1 seek=$(($func_file_offset)) conv=notrunc
+rm -f lp_patch.bin
--- tools/testing/selftests/riscv/cfi3/riscv_cfi_test.c
+++ tools/testing/selftests/riscv/cfi3/riscv_cfi_test.c
@@ -1,60 +1,224 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
+#include "../../kselftest.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <errno.h>
+
 #include <sys/prctl.h>
+#include <errno.h>
+#include <signal.h>
+#include <asm/ucontext.h>
+
+/* do not optimize cfi related test functions */
+#pragma GCC push_options
+#pragma GCC optimize("O0")                           // disable optmize
+#pragma GCC diagnostic ignored "-Wstringop-overflow" // disable overflow warning
+
+#define TEST_TYPE_LP_DISABLE 0
+#define TEST_TYPE_LP_ENABLE  1
+#define TEST_TYPE_SS_DISABLE 2
+#define TEST_TYPE_SS_ENABLE  3
+
+#ifndef SEGV_CPERR
+#define SEGV_CPERR	10	/* Control protection fault */
+#endif
 
 void print_usage(const char *prog_name)
 {
-	fprintf(stderr, "Usage: %s <cfi_type>\n", prog_name);
-	fprintf(stderr, "  lp  - Only enable LANDING_PAD\n");
-	fprintf(stderr, "  ss  - Only enable SHADOW_STACK\n");
+	printf("Usage: %s <cfi_type>\n", prog_name);
+	printf("  lp [0] - test LANDING_PAD\n");
+	printf("  ss [0] - test SHADOW_STACK\n");
+}
+
+struct test_ctx {
+	int test_mode;
+	int test_type;
+};
+static struct test_ctx g_ctx = {0};
+
+static int parse_args(int argc, char **argv)
+{
+	if (argc < 2 || argc > 3 ||
+	    (strcmp(argv[1], "lp") != 0 && strcmp(argv[1], "ss") != 0) ||
+	    ((argc == 3) && (strcmp(argv[2], "0") != 0 && strcmp(argv[2], "1") != 0))) {
+		return -1;
+	}
+
+	if (strcmp(argv[1], "lp") == 0) {
+		if (argc == 3 && strcmp(argv[2], "0") == 0) {
+			g_ctx.test_type = TEST_TYPE_LP_DISABLE;
+			printf("test_type = TEST_TYPE_LP_DISABLE(%d)\n", TEST_TYPE_LP_DISABLE);
+		} else {
+			g_ctx.test_type = TEST_TYPE_LP_ENABLE;
+			printf("test_type = TEST_TYPE_LP_ENABLE(%d)\n", TEST_TYPE_LP_ENABLE);
+		}
+	} else {
+		if (argc == 3 && strcmp(argv[2], "0") == 0) {
+			g_ctx.test_type = TEST_TYPE_SS_DISABLE;
+			printf("test_type = TEST_TYPE_SS_DISABLE(%d)\n", TEST_TYPE_SS_DISABLE);
+		} else {
+			g_ctx.test_type = TEST_TYPE_SS_ENABLE;
+			printf("test_type = TEST_TYPE_SS_ENABLE(%d)\n", TEST_TYPE_SS_ENABLE);
+		}
+	}
+
+	return 0;
+}
+
+typedef int (*TestFuncPtr)(void);
+static int test_func0(void)
+{
+	return 0;
+}
+
+static int test_func1(void)
+{
+	return 1;
+}
+
+static TestFuncPtr g_func_array[] = {
+	test_func0,
+	test_func1,
+};
+
+static int test_lp_disable(void)
+{
+	int func_id = 1;
+	int ret;
+
+	prctl(PR_SET_INDIR_BR_LP_STATUS, 0, 0, 0, 0);
+	if (func_id == 0)
+		ret = g_func_array[0]();
+	else
+		ret = g_func_array[1]();
+
+	return ret;
+}
+
+static int test_lp_enable(void)
+{
+	int func_id = 0;
+	int ret;
+
+	prctl(PR_SET_INDIR_BR_LP_STATUS, PR_INDIR_BR_LP_ENABLE, 0, 0, 0);
+
+	if (func_id == 0)
+		ret = g_func_array[0]();
+	else
+		ret = g_func_array[1]();
+
+	prctl(PR_SET_INDIR_BR_LP_STATUS, 0, 0, 0, 0);
+	return ret;
+}
+
+static int test_protect_return(void)
+{
+	char local_buf[32];
+
+	memset(local_buf, 0, 64);	// stack overflow attach
+	return 0;
+}
+
+static void sigsegv_handler(int signum, siginfo_t *si, void *uc)
+{
+	int exit_code = 128 + SIGSEGV;
+
+	prctl(PR_SET_INDIR_BR_LP_STATUS, 0, 0, 0, 0);
+	prctl(PR_SET_SHADOW_STACK_STATUS, 0, 0, 0, 0);
+
+	struct ucontext *ctx = (struct ucontext *) uc;
+
+	if (si->si_code == SEGV_CPERR) {
+		printf("Control flow violation happened somewhere\n");
+		printf("pc where violation happened %lx\n", ctx->uc_mcontext.gregs[0]);
+
+		if (g_ctx.test_type == TEST_TYPE_LP_ENABLE ||
+		    g_ctx.test_type == TEST_TYPE_SS_ENABLE) {
+			printf("Test(%d) pass, attack detected\n", g_ctx.test_type);
+			ksft_cnt.ksft_pass += 1;
+			exit_code = 0;
+		}
+	}
+
+	if (g_ctx.test_type == TEST_TYPE_SS_DISABLE) {
+		printf("Test(%d) pass, crash detected\n", g_ctx.test_type);
+		ksft_cnt.ksft_pass += 1;
+		exit_code = 0;
+	}
+
+	ksft_print_msg("%s: %u / %u tests passed.\n",
+		ksft_cnt.ksft_pass == ksft_plan ? "PASSED" : "FAILED",
+		ksft_cnt.ksft_pass, ksft_plan);
+	ksft_finished();
+
+	exit(exit_code);
+}
+
+static bool register_signal_handler(void)
+{
+	struct sigaction sa = {};
+
+	sa.sa_sigaction = sigsegv_handler;
+	sa.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGSEGV, &sa, NULL))
+		return false;
+
+	return true;
 }
 
 int main(int argc, char *argv[])
 {
 	int ret;
-	int cfi_type = -1;
-	unsigned long status = 0;
 
-	if (argc != 2) {
+	ksft_print_header();
+	ksft_set_plan(1);
+	ksft_print_msg("starting risc-v cfi tests\n");
+
+	ret = parse_args(argc, argv);
+	if (ret != 0) {
 		print_usage(argv[0]);
-		return EXIT_FAILURE;
+		return EXIT_FAILURE;	// TODO: ksft_finished()
 	}
 
-	if (strcmp(argv[1], "lp") == 0) {
-		cfi_type = PR_SET_INDIR_BR_LP_STATUS;
-	} else if (strcmp(argv[1], "ss") == 0) {
-		cfi_type = PR_SET_SHADOW_STACK_STATUS;
-	} else {
-		fprintf(stderr, "Error: Invalid argument '%s' (enable all)\n", argv[1]);
-		print_usage(argv[0]);
+	if (!register_signal_handler()) {
+		printf("registering signal handler for SIGSEGV failed\n");
+		return -1;
 	}
 
-	if (cfi_type == PR_SET_INDIR_BR_LP_STATUS) {
-		prctl(PR_SET_INDIR_BR_LP_STATUS, PR_INDIR_BR_LP_ENABLE, 0, 0, 0);
+	if (g_ctx.test_type == TEST_TYPE_LP_DISABLE) {
+		printf("Test(%d) indirect jump without landing pad protect\n",
+			g_ctx.test_type);
+		test_lp_disable();	// return back here normally
+		printf("Test(%d) pass, return normallly\n",
+			g_ctx.test_type);
+		ksft_cnt.ksft_pass += 1;
+	}
+	if (g_ctx.test_type == TEST_TYPE_LP_ENABLE) {
+		printf("Test(%d) indirect jump with landing pad protect\n",
+			g_ctx.test_type);
+		test_lp_enable();	// get SIGSEGV and handle in sigsegv_handler()
+	}
+
+	if (g_ctx.test_type == TEST_TYPE_SS_DISABLE) {
+		printf("Test(%d) return stack without shadow stack protect\n",
+			g_ctx.test_type);
 		prctl(PR_SET_SHADOW_STACK_STATUS, 0, 0, 0, 0);
-	} else if (cfi_type == PR_SET_SHADOW_STACK_STATUS) {
-		prctl(PR_SET_INDIR_BR_LP_STATUS, 0, 0, 0, 0);
+		test_protect_return();	// get SIGSEGV and handle in sigsegv_handler()
+	}
+	if (g_ctx.test_type == TEST_TYPE_SS_ENABLE) {
+		printf("Test(%d) return stack with shadow stack protect\n",
+			g_ctx.test_type);
 		prctl(PR_SET_SHADOW_STACK_STATUS, PR_SHADOW_STACK_ENABLE, 0, 0, 0);
-	} else {
-		prctl(PR_SET_INDIR_BR_LP_STATUS, PR_INDIR_BR_LP_ENABLE, 0, 0, 0);
-		prctl(PR_SET_SHADOW_STACK_STATUS, PR_SHADOW_STACK_ENABLE, 0, 0, 0);
+		test_protect_return();	// get SIGSEGV and handle in sigsegv_handler()
+		prctl(PR_SET_SHADOW_STACK_STATUS, 0, 0, 0, 0);
 	}
 
-	for (int i = 0; i < 10; i++) {
-		ret = prctl(PR_GET_INDIR_BR_LP_STATUS, &status, 0, 0, 0);
-		printf("PR_GET_INDIR_BR_LP_STATUS: %s\n",
-			status&PR_INDIR_BR_LP_ENABLE ? "enable" : "disable");
-		ret = prctl(PR_GET_SHADOW_STACK_STATUS, &status, 0, 0, 0);
-		printf("PR_GET_SHADOW_STACK_STATUS: %s\n",
-			status&PR_SHADOW_STACK_ENABLE ? "enable" : "disable");
-
-		sleep(1);
-	}
-
-	return EXIT_SUCCESS;
+	ksft_print_msg("%s: %u / %u tests passed.\n",
+			ksft_cnt.ksft_pass == ksft_plan ? "PASSED" : "FAILED",
+			ksft_cnt.ksft_pass, ksft_plan);
+	ksft_finished();
 }
+
+#pragma GCC pop_options
