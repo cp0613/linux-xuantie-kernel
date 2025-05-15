b797fc6c1fa1a63b6a281d4b03b09098112f5170
Yury Norov [NVIDIA] <yury.norov@gmail.com>
2025-05-15 16:58:31 +0000

    cpumask: Relax cpumask_any_but()
    Similarly to other cpumask search functions, accept -1, and consider it as
    'any CPU' hint. This helps users to avoid coding special cases.
    
    Signed-off-by: Yury Norov [NVIDIA] <yury.norov@gmail.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: James Morse <james.morse@arm.com>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Reviewed-by: Fenghua Yu <fenghuay@nvidia.com>
    Tested-by: James Morse <james.morse@arm.com>
    Tested-by: Tony Luck <tony.luck@intel.com>
    Tested-by: Fenghua Yu <fenghuay@nvidia.com>
    Link: https://lore.kernel.org/20250515165855.31452-2-james.morse@arm.com
    (cherry picked from commit 189572bf4e005431051319def435ac4b7e4489ca)
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -390,14 +390,18 @@ unsigned int cpumask_next_wrap(int n, const struct cpumask *src)
  * @cpu: the cpu to ignore.
  *
  * Often used to find any cpu but smp_processor_id() in a mask.
- * Returns >= nr_cpu_ids if no cpus set.
+ * If @cpu == -1, the function is equivalent to cpumask_any().
+ * Return: >= nr_cpu_ids if no cpus set.
  */
-static inline
-unsigned int cpumask_any_but(const struct cpumask *mask, unsigned int cpu)
+static __always_inline
+unsigned int cpumask_any_but(const struct cpumask *mask, int cpu)
 {
 	unsigned int i;
 
-	cpumask_check(cpu);
+	/* -1 is a legal arg here. */
+	if (cpu != -1)
+		cpumask_check(cpu);
+
 	for_each_cpu(i, mask)
 		if (i != cpu)
 			break;
@@ -405,7 +409,34 @@ unsigned int cpumask_any_but(const struct cpumask *mask, unsigned int cpu)
 }
 
 /**
- * cpumask_nth - get the first cpu in a cpumask
+ * cpumask_any_and_but - pick an arbitrary cpu from *mask1 & *mask2, but not this one.
+ * @mask1: the first input cpumask
+ * @mask2: the second input cpumask
+ * @cpu: the cpu to ignore
+ *
+ * If @cpu == -1, the function is equivalent to cpumask_any_and().
+ * Returns >= nr_cpu_ids if no cpus set.
+ */
+static __always_inline
+unsigned int cpumask_any_and_but(const struct cpumask *mask1,
+				 const struct cpumask *mask2,
+				 int cpu)
+{
+	unsigned int i;
+
+	/* -1 is a legal arg here. */
+	if (cpu != -1)
+		cpumask_check(cpu);
+
+	i = cpumask_first_and(mask1, mask2);
+	if (i != cpu)
+		return i;
+
+	return cpumask_next_and(cpu, mask1, mask2);
+}
+
+/**
+ * cpumask_nth - get the Nth cpu in a cpumask
  * @srcp: the cpumask pointer
  * @cpu: the N'th cpu to find, starting from 0
  *
