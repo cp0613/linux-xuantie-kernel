424bfd67c7c343a710761c3bd4850b0d967f6635
Guo Ren (Alibaba DAMO Academy) <guoren.gr@alibaba-inc.com>
2025-09-25 17:06:34 +0800

    RISC-V: KVM: Fixup guest tpe usage broken
    According the spec:
    
    When V=1, both vsstatus.FS and the HS-level sstatus.FS are in
    effect. Attempts to execute a floating-point instruction when
    either field is 0 (Off) raise an illegal-instruction exception.
    Modifying the floating-point state when V=1 causes both fields
    to be set to 3 (Dirty).
    
    So, we can't force SR_MS zero in hsstatus csr.
    
    Signed-off-by: Guo Ren (Alibaba DAMO Academy) <guoren.gr@alibaba-inc.com>
    Change-Id: I6da4fb5b56d6467c3892b935681ea61f2ce7534c

--- arch/riscv/kvm/vcpu_matrix.c
+++ arch/riscv/kvm/vcpu_matrix.c
@@ -11,12 +11,12 @@ void kvm_riscv_vcpu_m_reset(struct kvm_vcpu *vcpu)
 {
 	struct kvm_cpu_context *cntx = &vcpu->arch.guest_context;
 
+	cntx->sstatus &= ~SR_MS;
+
 	if (has_matrix())
 		cntx->sstatus |= SR_MS_INITIAL;
 	else
 		cntx->sstatus |= SR_MS_OFF;
-
-	cntx->sstatus &= ~SR_MS;
 }
 
 static void kvm_riscv_vcpu_m_clean(struct kvm_cpu_context *cntx)
@@ -54,8 +54,7 @@ void kvm_riscv_vcpu_host_m_save(struct kvm_cpu_context *cntx)
 	if (!has_matrix())
 		return;
 
-	if ((cntx->sstatus & SR_MS) != SR_MS_OFF)
-		__riscv_m_mstate_save(&cntx->mstate, cntx->mstate.datap);
+	__riscv_m_mstate_save(&cntx->mstate, cntx->mstate.datap);
 }
 
 void kvm_riscv_vcpu_host_m_restore(struct kvm_cpu_context *cntx)
@@ -63,8 +62,7 @@ void kvm_riscv_vcpu_host_m_restore(struct kvm_cpu_context *cntx)
 	if (!has_matrix())
 		return;
 
-	if ((cntx->sstatus & SR_MS) != SR_MS_OFF)
-		__riscv_m_mstate_restore(&cntx->mstate, cntx->mstate.datap);
+	__riscv_m_mstate_restore(&cntx->mstate, cntx->mstate.datap);
 }
 
 int kvm_riscv_vcpu_alloc_m_context(struct kvm_vcpu *vcpu,
