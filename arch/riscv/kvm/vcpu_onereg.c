bc97dd5e2aee2bb0e0f3b831ad9065c121163aa9
Xu Lu <luxu.kernel@bytedance.com>
2025-10-27 15:10:34 +0800

    RISC-V: KVM: Allow Zalasr extensions for Guest/VM
    Extend the KVM ISA extension ONE_REG interface to allow KVM user space
    to detect and enable Zalasr extensions for Guest/VM.
    
    Signed-off-by: Xu Lu <luxu.kernel@bytedance.com>
    
    Change-Id: I443475254b70a4a002dd3abe2795ea9e878009fe

--- arch/riscv/kvm/vcpu_onereg.c
+++ arch/riscv/kvm/vcpu_onereg.c
@@ -49,6 +49,7 @@ static const unsigned long kvm_isa_ext_arr[] = {
 	KVM_ISA_EXT_ARR(SVVPTC),
 	KVM_ISA_EXT_ARR(ZABHA),
 	KVM_ISA_EXT_ARR(ZACAS),
+	KVM_ISA_EXT_ARR(ZALASR),
 	KVM_ISA_EXT_ARR(ZAWRS),
 	KVM_ISA_EXT_ARR(ZBA),
 	KVM_ISA_EXT_ARR(ZBB),
@@ -137,6 +138,7 @@ static bool kvm_riscv_vcpu_isa_disable_allowed(unsigned long ext)
 	case KVM_RISCV_ISA_EXT_SVNAPOT:
 	case KVM_RISCV_ISA_EXT_ZABHA:
 	case KVM_RISCV_ISA_EXT_ZACAS:
+	case KVM_RISCV_ISA_EXT_ZALASR:
 	case KVM_RISCV_ISA_EXT_ZAWRS:
 	case KVM_RISCV_ISA_EXT_ZBA:
 	case KVM_RISCV_ISA_EXT_ZBB:
